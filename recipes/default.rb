da232f28c735a1e61c5128f642910ab0c0cb2bc0
Stephen Lauck <stephen.lauck@gmail.com>
2015-02-02 14:56:39 -0500

    add default code for chef-server-core

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,16 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+
+directory '/etc/opscode' do
+  mode 0755
+  recursive true
+end
+
+directory '/etc/opscode-analytics' do
+  recursive true
+end
+
+chef_server_ingredient 'chef-server-core' do
+  notifies :reconfigure, 'chef_server_ingredient[chef-server-core]'
+end
