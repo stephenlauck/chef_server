11fab32a2d4e815d5c71ff7da573de6cf5ba6127
Stephen Lauck <stephen.lauck@gmail.com>
2015-02-02 23:23:42 -0500

    add lwrp for creating orgs and users

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,11 +1,1 @@
-default['chef_server']['admin']['username'] = 'flock'
-default['chef_server']['admin']['firstname'] = 'Florian'
-default['chef_server']['admin']['lastname'] = 'Lock'
-default['chef_server']['admin']['email'] = 'ops@example.com'
-default['chef_server']['admin']['password'] = 'DontUseThis4Real'
-default['chef_server']['admin']['private_key_path'] = '/tmp/flock.pem'
 
-default['chef_server']['organization'] = 'example'
-default['chef_server']['organization_long_name'] = 'Example Organization'
-default['chef_server']['organization_private_key'] = "#{node['chef_server']['organization']}-validator.pem"
-default['chef_server']['organization_private_key_path'] = File.join('/tmp', node['chef_server']['organization_private_key'])
--- recipes/default.rb
+++ recipes/default.rb
@@ -13,6 +13,26 @@ directory '/etc/opscode-analytics' do
   recursive true
 end
 
-chef_server_ingredient 'chef-server-core' do
+chef_server_ingredient 'chef-server-core'
+
+chef_server_user 'flock' do
+  firstname 'Florian'
+  lastname 'Lock'
+  email 'ops@example.com'
+  password 'DontUseThis4Real'
+  private_key_path '/tmp/flock.pem'
+  action :create
   notifies :reconfigure, 'chef_server_ingredient[chef-server-core]'
+end
+
+chef_server_org 'example' do
+  org_long_name 'Example Organization'
+  org_private_key_path '/tmp/example-validator.pem'
+  action :create
+  notifies :reconfigure, 'chef_server_ingredient[chef-server-core]'
+end
+
+chef_server_org 'example' do
+  admins %w{ flock }
+  action :add_admin
 end
