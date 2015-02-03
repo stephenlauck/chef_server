7ae88e5bb0ee8e3a10367b8f95122d139de28cc9
Stephen Lauck <stephen.lauck@gmail.com>
2015-02-02 20:12:20 -0500

    remove bad char

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,7 @@ default['chef_server']['admin']['firstname'] = 'Florian'
 default['chef_server']['admin']['lastname'] = 'Lock'
 default['chef_server']['admin']['email'] = 'ops@example.com'
 default['chef_server']['admin']['password'] = 'DontUseThis4Real'
-default['chef_server']['admin']['private_key_path'] = '/tmp/flock.pem'_
+default['chef_server']['admin']['private_key_path'] = '/tmp/flock.pem'
 
 default['chef_server']['organization'] = 'example'
 default['chef_server']['organization_long_name'] = 'Example Organization'
