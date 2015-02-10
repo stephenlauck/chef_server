e5cfc56a92184cc02d31ee1c05ede61f505cfc18
Stephen Lauck <stephen.lauck@gmail.com>
2015-02-10 17:26:36 -0500

    add templates for analytics settings from chef server

--- recipes/analytics.rb
+++ recipes/analytics.rb
@@ -13,3 +13,18 @@ template "/etc/opscode-analytics/opscode-analytics.rb" do
   )
   notifies :reconfigure, 'chef_server_ingredient[opscode-analytics]'
 end
+
+template "/etc/opscode-analytics/actions-source.json" do
+  source "action-source.json.erb"
+  variables(
+
+  )
+  notifies :reconfigure, 'chef_server_ingredient[opscode-analytics]'
+end
+
+cookbook_file "/etc/opscode-analytics/webui_priv.pem" do
+  source "webui_priv.pem"
+  mode 0600
+  action :create
+  notifies :reconfigure, 'chef_server_ingredient[opscode-analytics]'
+end
